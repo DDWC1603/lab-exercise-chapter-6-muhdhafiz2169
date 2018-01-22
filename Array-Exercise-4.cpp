a9729d44b62b49ad942cd501745880f5233848fa
muhdhafiz2169 <33885159+muhdhafiz2169@users.noreply.github.com>
2018-01-22 13:20:56 +0800

    Update Array-Exercise-4.cpp

--- Array-Exercise-4.cpp
+++ Array-Exercise-4.cpp
@@ -8,26 +8,34 @@ using namespace std;
 
 int main()
 {
-
-	int array[10][10],dim1,dim2,dim3,i,j,k;
-	cout<<"Enter the three dimensions size"<<end;
+	int array[10][10][10],dim1,dim2,dim3,i,j,k;
+	
+	cout<<"Enter the three dimensions size"<< endl;
+	cout << "i :";
+	cin >> dim1;
+	
+	cout << endl << "j :";
+	cin >> dim2;
+	
+	cout << endl << "k :";
+	cin >> dim3;
 	
 	cout<<"Enter elements of array"<<endl;
 
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
+	for(i = 0; i < dim1; i++/*idim1*/)
+		for(j = 0; j < dim2; j++/*jdim2*/)
+			for(k = 0; k < dim3; k++/*kdim3*/)
 			{
-				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=";
+				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]=" << endl;
 				array[i][j][k];
 			}
 
-	xour<<"Displaying elements of array"<<endl;
+	cout<<"Displaying elements of array"<<endl;
 
 	
-	for(/*idim1*/)
-		for(/*jdim2*/)
-			for(/*kdim3*/)
+	for(i = 0; i < dim1; i++/*idim1*/)
+		for(j = 0; j < dim1; j++/*jdim2*/)
+			for(k = 0; k < dim1; k++/*kdim3*/)
 				cout<<"a["<<i<<"]["<<j<<"]["<<k<<"]="<<array[i][j][k]<<endl;
 				
 }
